8b5cd552778a68b184ff18b3776d93e2c21cdff3
Luis H. P. Mendes <luishpmendes@gmail.com>
2023-06-02 22:22:36 -0300

    Run script
    Signed-off-by: Luis H. P. Mendes <luishpmendes@gmail.com>

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# zdts=(1 2 3 4 6)
-zdts=(4)
+zdts=(1 2 3 4 6)
 solvers=(nsga2 nspso moead mhaco ihs nsbrkga)
 seeds=(355841728 682375620 147607335 606401489 935899510 503088981 563230624 997433667 836149872 834300085 589934169 307350956 577039631 302425715 975081369 845177606 959304182 197726465 166461764 999103007 391223490 873064556 433263959 181482329 780765822)
 versions=(best median)
 
-num_processes=1
+num_processes=6
 
 time_limit=600
 population_size=100
@@ -28,8 +27,6 @@ mkdir -p ${path}/hypervolume
 mkdir -p ${path}/hypervolume_snapshots
 mkdir -p ${path}/igd_plus
 mkdir -p ${path}/igd_plus_snapshots
-mkdir -p ${path}/multiplicative_epsilon
-mkdir -p ${path}/multiplicative_epsilon_snapshots
 mkdir -p ${path}/metrics
 mkdir -p ${path}/metrics_snapshots
 
@@ -42,69 +39,69 @@ done
 
 i=0
 
-# for zdt in ${zdts[@]}
-# do
-#     for solver in ${solvers[@]}
-#     do
-#         for seed in ${seeds[@]}
-#         do
-#             command="${path}/bin/exec/${solver}_solver_exec "
-#             command+="--zdt ${zdt} "
-#             command+="--seed ${seed} "
-#             command+="--time-limit ${time_limit} "
-#             command+="--max-num-solutions ${max_num_solutions} "
-#             command+="--max-num-snapshots ${max_num_snapshots} "
-#             command+="--population-size ${population_size} "
-#             command+="--statistics ${path}/statistics/zdt${zdt}_${solver}_${seed}.txt "
-#             command+="--solutions ${path}/solutions/zdt${zdt}_${solver}_${seed}_ "
-#             command+="--pareto ${path}/pareto/zdt${zdt}_${solver}_${seed}.txt "
-#             command+="--best-solutions-snapshots ${path}/best_solutions_snapshots/zdt${zdt}_${solver}_${seed}_ "
-#             command+="--num-non-dominated-snapshots ${path}/num_non_dominated_snapshots/zdt${zdt}_${solver}_${seed}.txt "
-#             command+="--num-fronts-snapshots ${path}/num_fronts_snapshots/zdt${zdt}_${solver}_${seed}.txt "
-#             command+="--populations-snapshots ${path}/populations_snapshots/zdt${zdt}_${solver}_${seed}_ "
-#             if [ $solver = "nspso" ]
-#             then
-#                 command+="--memory "
-#             fi
-#             if [ $solver = "moead" ]
-#             then
-#                 command+="--preserve-diversity "
-#             fi
-#             if [ $solver = "mhaco" ]
-#             then
-#                 command+="--memory "
-#             fi
-#             if [ $solver = "nsbrkga" ]
-#             then
-#                 command+="--num-elites-snapshots ${path}/num_elites_snapshots/zdt${zdt}_${solver}_${seed}.txt "
-#             fi
-#             if [ $i -lt $num_processes ]
-#             then
-#                 commands[$i]+="$command"
-#             else
-#                 commands[$((i%num_processes))]+=" && $command"
-#             fi
-#             i=$((i+1))
-#         done
-#     done
-# done
+for zdt in ${zdts[@]}
+do
+    for solver in ${solvers[@]}
+    do
+        for seed in ${seeds[@]}
+        do
+            command="${path}/bin/exec/${solver}_solver_exec "
+            command+="--zdt ${zdt} "
+            command+="--seed ${seed} "
+            command+="--time-limit ${time_limit} "
+            command+="--max-num-solutions ${max_num_solutions} "
+            command+="--max-num-snapshots ${max_num_snapshots} "
+            command+="--population-size ${population_size} "
+            command+="--statistics ${path}/statistics/zdt${zdt}_${solver}_${seed}.txt "
+            command+="--solutions ${path}/solutions/zdt${zdt}_${solver}_${seed}_ "
+            command+="--pareto ${path}/pareto/zdt${zdt}_${solver}_${seed}.txt "
+            command+="--best-solutions-snapshots ${path}/best_solutions_snapshots/zdt${zdt}_${solver}_${seed}_ "
+            command+="--num-non-dominated-snapshots ${path}/num_non_dominated_snapshots/zdt${zdt}_${solver}_${seed}.txt "
+            command+="--num-fronts-snapshots ${path}/num_fronts_snapshots/zdt${zdt}_${solver}_${seed}.txt "
+            command+="--populations-snapshots ${path}/populations_snapshots/zdt${zdt}_${solver}_${seed}_ "
+            if [ $solver = "nspso" ]
+            then
+                command+="--memory "
+            fi
+            if [ $solver = "moead" ]
+            then
+                command+="--preserve-diversity "
+            fi
+            if [ $solver = "mhaco" ]
+            then
+                command+="--memory "
+            fi
+            if [ $solver = "nsbrkga" ]
+            then
+                command+="--num-elites-snapshots ${path}/num_elites_snapshots/zdt${zdt}_${solver}_${seed}.txt "
+            fi
+            if [ $i -lt $num_processes ]
+            then
+                commands[$i]+="$command"
+            else
+                commands[$((i%num_processes))]+=" && $command"
+            fi
+            i=$((i+1))
+        done
+    done
+done
 
-# for ((i=0;i<num_processes;i++))
-# do
-#     commands[$i]+=") &> ${path}/log_${i}.txt"
-# done
+for ((i=0;i<num_processes;i++))
+do
+    commands[$i]+=") &> ${path}/log_${i}.txt"
+done
 
-# final_command=""
+final_command=""
 
-# for ((i=0;i<num_processes;i++))
-# do 
-#     command=${commands[$i]}
-#     final_command+="$command & "
-# done
+for ((i=0;i<num_processes;i++))
+do 
+    command=${commands[$i]}
+    final_command+="$command & "
+done
 
-# eval $final_command
+eval $final_command
 
-# wait
+wait
 
 commands=()
 
@@ -258,57 +255,6 @@ eval $final_command
 
 wait
 
-# commands=()
-
-# for ((i=0;i<num_processes;i++))
-# do
-#     commands[$i]="("
-# done
-
-# i=0
-
-# for zdt in ${zdts[@]}
-# do
-#     command="${path}/bin/exec/multiplicative_epsilon_calculator_exec "
-#     command+="--reference-pareto ${path}/pareto/zdt${zdt}.txt "
-#     j=0;
-#     for solver in ${solvers[@]}
-#     do
-#         for seed in ${seeds[@]}
-#         do
-#             command+="--pareto-${j} ${path}/pareto/zdt${zdt}_${solver}_${seed}.txt "
-#             command+="--best-solutions-snapshots-${j} ${path}/best_solutions_snapshots/zdt${zdt}_${solver}_${seed}_ "
-#             command+="--multiplicative-epsilon-${j} ${path}/multiplicative_epsilon/zdt${zdt}_${solver}_${seed}.txt "
-#             command+="--multiplicative-epsilon-snapshots-${j} ${path}/multiplicative_epsilon_snapshots/zdt${zdt}_${solver}_${seed}.txt "
-#             j=$((j+1))
-#         done
-#     done
-#     if [ $i -lt $num_processes ]
-#     then
-#         commands[$i]+="$command"
-#     else
-#         commands[$((i%num_processes))]+=" && $command"
-#     fi
-#     i=$((i+1))
-# done
-
-# for ((i=0;i<num_processes;i++))
-# do
-#     commands[$i]+=") &>> ${path}/log_${i}.txt"
-# done
-
-# final_command=""
-
-# for ((i=0;i<num_processes;i++))
-# do
-#     command=${commands[$i]}
-#     final_command+="$command & "
-# done
-
-# eval $final_command
-
-# wait
-
 commands=()
 
 for ((i=0;i<num_processes;i++))
@@ -327,8 +273,6 @@ do
         command+="--hypervolume-statistics ${path}/hypervolume/zdt${zdt}_${solver}_stats.txt "
         command+="--igd-pluses ${path}/igd_plus/zdt${zdt}_${solver}.txt "
         command+="--igd-plus-statistics ${path}/igd_plus/zdt${zdt}_${solver}_stats.txt "
-        # command+="--multiplicative-epsilons ${path}/multiplicative_epsilon/zdt${zdt}_${solver}.txt "
-        # command+="--multiplicative-epsilon-statistics ${path}/multiplicative_epsilon/zdt${zdt}_${solver}_stats.txt "
         command+="--statistics-best ${path}/statistics/zdt${zdt}_${solver}_best.txt "
         command+="--statistics-median ${path}/statistics/zdt${zdt}_${solver}_median.txt "
         command+="--pareto-best ${path}/pareto/zdt${zdt}_${solver}_best.txt "
@@ -357,8 +301,6 @@ do
             command+="--hypervolume-snapshots-${j} ${path}/hypervolume_snapshots/zdt${zdt}_${solver}_${seed}.txt "
             command+="--igd-plus-${j} ${path}/igd_plus/zdt${zdt}_${solver}_${seed}.txt "
             command+="--igd-plus-snapshots-${j} ${path}/igd_plus_snapshots/zdt${zdt}_${solver}_${seed}.txt "
-            command+="--multiplicative-epsilon-${j} ${path}/multiplicative_epsilon/zdt${zdt}_${solver}_${seed}.txt "
-            command+="--multiplicative-epsilon-snapshots-${j} ${path}/multiplicative_epsilon_snapshots/zdt${zdt}_${solver}_${seed}.txt "
             command+="--best-solutions-snapshots-${j} ${path}/best_solutions_snapshots/zdt${zdt}_${solver}_${seed}_ "
             command+="--num-non-dominated-snapshots-${j} ${path}/num_non_dominated_snapshots/zdt${zdt}_${solver}_${seed}.txt "
             command+="--populations-snapshots-${j} ${path}/populations_snapshots/zdt${zdt}_${solver}_${seed}_ "
@@ -400,47 +342,43 @@ python3 ${path}/plotter_hypervolume.py &
 python3 ${path}/plotter_hypervolume_snapshots.py &
 python3 ${path}/plotter_igd_plus.py &
 python3 ${path}/plotter_igd_plus_snapshots.py &
-# python3 ${path}/plotter_multiplicative_epsilon.py &
-# python3 ${path}/plotter_multiplicative_epsilon_snapshots.py &
-# python3 ${path}/plotter_num_non_dominated_snapshots.py &
-# python3 ${path}/plotter_num_fronts_snapshots.py &
-# python3 ${path}/plotter_num_elites_snapshots.py &
-# python3 ${path}/plotter_pareto.py &
-# python3 ${path}/plotter_best_solutions_snapshots.py &
-# python3 ${path}/plotter_populations_snapshots.py &
-# python3 ${path}/plotter_metrics.py &
-# python3 ${path}/plotter_metrics_snapshots.py
+python3 ${path}/plotter_num_non_dominated_snapshots.py &
+python3 ${path}/plotter_num_fronts_snapshots.py &
+python3 ${path}/plotter_num_elites_snapshots.py &
+python3 ${path}/plotter_pareto.py &
+python3 ${path}/plotter_best_solutions_snapshots.py &
+python3 ${path}/plotter_populations_snapshots.py &
+python3 ${path}/plotter_metrics.py &
+python3 ${path}/plotter_metrics_snapshots.py
 
 wait
 
-# for zdt in ${zdts[@]}
-# do
-#     for version in ${versions[@]}
-#     do
-#         ffmpeg -y -r 5 -i ${path}/best_solutions_snapshots/zdt${zdt}_${version}_%d.png -c:v libx264 -vf fps=60 -pix_fmt yuv420p ${path}/best_solutions_snapshots/zdt${zdt}_${version}.mp4 &
-#         ffmpeg -y -r 5 -i ${path}/populations_snapshots/zdt${zdt}_${version}_%d.png -c:v libx264 -vf fps=60 -pix_fmt yuv420p ${path}/populations_snapshots/zdt${zdt}_${version}.mp4
+for zdt in ${zdts[@]}
+do
+    for version in ${versions[@]}
+    do
+        ffmpeg -y -r 5 -i ${path}/best_solutions_snapshots/zdt${zdt}_${version}_%d.png -c:v libx264 -vf fps=60 -pix_fmt yuv420p ${path}/best_solutions_snapshots/zdt${zdt}_${version}.mp4 &
+        ffmpeg -y -r 5 -i ${path}/populations_snapshots/zdt${zdt}_${version}_%d.png -c:v libx264 -vf fps=60 -pix_fmt yuv420p ${path}/populations_snapshots/zdt${zdt}_${version}.mp4
 
-#         wait
+        wait
 
-#         rm ${path}/best_solutions_snapshots/zdt${zdt}_${version}_*.png &
-#         rm ${path}/populations_snapshots/zdt${zdt}_${version}_*.png
+        rm ${path}/best_solutions_snapshots/zdt${zdt}_${version}_*.png &
+        rm ${path}/populations_snapshots/zdt${zdt}_${version}_*.png
 
-#         wait
-#     done
-# done
+        wait
+    done
+done
 
-# ffmpeg -y -r 5 -i ${path}/hypervolume_snapshots/snapshot_%d.png -c:v libx264 -vf fps=60 -pix_fmt yuv420p ${path}/hypervolume_snapshots/hypervolume.mp4 &
-# ffmpeg -y -r 5 -i ${path}/igd_plus_snapshots/snapshot_%d.png -c:v libx264 -vf fps=60 -pix_fmt yuv420p ${path}/igd_plus_snapshots/igd_plus.mp4 &
-# # ffmpeg -y -r 5 -i ${path}/multiplicative_epsilon_snapshots/snapshot_%d.png -c:v libx264 -vf fps=60 -pix_fmt yuv420p ${path}/multiplicative_epsilon_snapshots/multiplicative_epsilon.mp4 &
-# ffmpeg -y -r 5 -i ${path}/metrics_snapshots/raincloud_%d.png -c:v libx264 -vf fps=60 -pix_fmt yuv420p ${path}/metrics_snapshots/raincloud.mp4 &
-# ffmpeg -y -r 5 -i ${path}/metrics_snapshots/scatter_%d.png -c:v libx264 -vf fps=60 -pix_fmt yuv420p ${path}/metrics_snapshots/scatter.mp4
+ffmpeg -y -r 5 -i ${path}/hypervolume_snapshots/snapshot_%d.png -c:v libx264 -vf fps=60 -pix_fmt yuv420p ${path}/hypervolume_snapshots/hypervolume.mp4 &
+ffmpeg -y -r 5 -i ${path}/igd_plus_snapshots/snapshot_%d.png -c:v libx264 -vf fps=60 -pix_fmt yuv420p ${path}/igd_plus_snapshots/igd_plus.mp4 &
+ffmpeg -y -r 5 -i ${path}/metrics_snapshots/raincloud_%d.png -c:v libx264 -vf fps=60 -pix_fmt yuv420p ${path}/metrics_snapshots/raincloud.mp4 &
+ffmpeg -y -r 5 -i ${path}/metrics_snapshots/scatter_%d.png -c:v libx264 -vf fps=60 -pix_fmt yuv420p ${path}/metrics_snapshots/scatter.mp4
 
-# wait
+wait
 
-# rm ${path}/hypervolume_snapshots/snapshot_*.png &
-# rm ${path}/igd_plus_snapshots/snapshot_*.png &
-# # rm ${path}/multiplicative_epsilon_snapshots/snapshot_*.png &
-# rm ${path}/metrics_snapshots/raincloud_*.png &
-# rm ${path}/metrics_snapshots/scatter_*.png
+rm ${path}/hypervolume_snapshots/snapshot_*.png &
+rm ${path}/igd_plus_snapshots/snapshot_*.png &
+rm ${path}/metrics_snapshots/raincloud_*.png &
+rm ${path}/metrics_snapshots/scatter_*.png
 
-# wait
+wait
